14e23117aef478270cd05f5c71314146b3fc9ada
Hidenori Ikeda <hidikeda7630@gmail.com>
2014-02-12 23:56:25 +0900

    1

---
+++ MultiThreadSample/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace MultiThreadSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnThread = new System.Windows.Forms.Button();
+            this.btnThreadPool = new System.Windows.Forms.Button();
+            this.btnTask = new System.Windows.Forms.Button();
+            this.btnLambda = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnThread
+            // 
+            this.btnThread.Location = new System.Drawing.Point(12, 12);
+            this.btnThread.Name = "btnThread";
+            this.btnThread.Size = new System.Drawing.Size(75, 23);
+            this.btnThread.TabIndex = 0;
+            this.btnThread.Text = "Thread";
+            this.btnThread.UseVisualStyleBackColor = true;
+            this.btnThread.Click += new System.EventHandler(this.btnThread_Click);
+            // 
+            // btnThreadPool
+            // 
+            this.btnThreadPool.Location = new System.Drawing.Point(12, 62);
+            this.btnThreadPool.Name = "btnThreadPool";
+            this.btnThreadPool.Size = new System.Drawing.Size(75, 23);
+            this.btnThreadPool.TabIndex = 1;
+            this.btnThreadPool.Text = "ThreadPool";
+            this.btnThreadPool.UseVisualStyleBackColor = true;
+            this.btnThreadPool.Click += new System.EventHandler(this.btnThreadPool_Click);
+            // 
+            // btnTask
+            // 
+            this.btnTask.Location = new System.Drawing.Point(12, 104);
+            this.btnTask.Name = "btnTask";
+            this.btnTask.Size = new System.Drawing.Size(75, 23);
+            this.btnTask.TabIndex = 2;
+            this.btnTask.Text = "Task";
+            this.btnTask.UseVisualStyleBackColor = true;
+            this.btnTask.Click += new System.EventHandler(this.btnTask_Click);
+            // 
+            // btnLambda
+            // 
+            this.btnLambda.Location = new System.Drawing.Point(12, 147);
+            this.btnLambda.Name = "btnLambda";
+            this.btnLambda.Size = new System.Drawing.Size(75, 23);
+            this.btnLambda.TabIndex = 3;
+            this.btnLambda.Text = "Lambda";
+            this.btnLambda.UseVisualStyleBackColor = true;
+            this.btnLambda.Click += new System.EventHandler(this.btnLambda_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 147);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLambda);
+            this.Controls.Add(this.btnTask);
+            this.Controls.Add(this.btnThreadPool);
+            this.Controls.Add(this.btnThread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnThread;
+        private System.Windows.Forms.Button btnThreadPool;
+        private System.Windows.Forms.Button btnTask;
+        private System.Windows.Forms.Button btnLambda;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MultiThreadSample/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadSample
+{
+    public partial class Form1 : Form
+    {
+        private int shareresult;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Thread
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnThread_Click(object sender, EventArgs e)
+        {
+            Thread t=new Thread(new ThreadStart(Dowork));
+            t.Start();
+        }
+
+        /// <summary>
+        /// ThreadPool
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnThreadPool_Click(object sender, EventArgs e)
+        {
+            object state = new object();
+
+            bool queReslt = ThreadPool.QueueUserWorkItem(new WaitCallback(Dowork),state);
+        }
+
+        /// <summary>
+        /// Task 4.5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTask_Click(object sender, EventArgs e)
+        {
+            //返り値がないTask
+            Action d = Dowork;
+            Task task = new Task(d);
+            task.Start();
+
+            //返り値があるTask
+            Func<int> dw = DoworkReturn;
+            Task<int> taskint = new Task<int>(dw);//非同期で実行するメソッドの返り値をTask<T>に設定する
+            taskint.Start();
+            int result = taskint.Result;//返り値がResultで返却される resultはスレッドの処理が終わるまでブロックされる
+
+            //非同期処理が完了した時点で、結果を引数として、継続して非同期処理を実行する その結果をUIに反映 UIスレッドはブロックしない
+            Task<int> taskint2 = Task.Run<int>(() => DoworkReturn())
+                                .ContinueWith<int>(t => DoworkReturn2(t.Result),TaskScheduler.FromCurrentSynchronizationContext());
+            
+        }
+        
+        /// <summary>
+        /// lambda
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLambda_Click(object sender, EventArgs e)
+        {
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        shareresult = i * j;
+                    }
+                }
+            });
+        }
+
+       
+        /// <summary>
+        /// 非同期処理メソッド
+        /// </summary>
+        private void Dowork()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    shareresult = i * j;
+                }
+            }
+        }
+
+        private void Dowork(object arg)
+        {
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    arg = i * j;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 非同期処理メソッド　int返却
+        /// </summary>
+        /// <returns></returns>
+        private int DoworkReturn()
+        {
+            int result = 0;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    result = i * j;
+                }
+            }
+
+            return result;
+        }
+
+        private int DoworkReturn2(int result)
+        {
+            label1.Text = result.ToString();
+            return result+1;
+        }
+
+        private void SetChekresult(int result)
+        {
+            label1.Text = result.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
