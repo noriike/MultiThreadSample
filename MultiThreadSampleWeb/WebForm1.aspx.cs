12d3a23bcb5a4932625bfdd9094212280410d6bb
Hidenori Ikeda <hidikeda7630@gmail.com>
2014-02-17 00:09:29 +0900

    2

---
+++ MultiThreadSampleWeb/WebForm1.aspx.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultiThreadSampleWeb
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        /// <summary>
+        /// RegisterAsyncTaskで非同期操作をページに登録
+        /// .aspxにAsync="true"属性追加
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            RegisterAsyncTask(new PageAsyncTask(LoadSomeData));
+        }
+
+        /// <summary>
+        /// async awaitで非同期処理
+        /// </summary>
+        /// <returns></returns>
+        public async Task LoadSomeData()
+        {
+            var result2 = Task.Run<int>(() => DoworkReturn());
+            var result = Task.Run<int>(() => DoworkReturnTyp2());
+
+            //すべての処理が完了するまで待機
+            await Task.WhenAll(result2, result);
+
+            DoworkReturn2(result2.Result);
+            DoworkReturn3(result.Result);
+        }
+
+        protected async void Button1_Click(object sender, EventArgs e)
+        {
+            int result2 = await Task.Run<int>(() => DoworkReturn());
+            DoworkReturn2(result2);
+        }
+
+        protected async void Button2_Click(object sender, EventArgs e)
+        {
+            int result = await Task.Run<int>(() => DoworkReturnTyp2());
+            DoworkReturn3(result);
+        }
+
+        /// <summary>
+        /// 非同期処理メソッド　int返却
+        /// </summary>
+        /// <returns></returns>
+        private int DoworkReturn()
+        {
+            int result = 0;
+
+            for (int i = 0; i < 1000000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    result = i * j;
+                }
+            }
+
+            return result;
+        }
+
+        private int DoworkReturnTyp2()
+        {
+            int result = 0;
+
+            for (int i = 0; i < 2000000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    result = i * j;
+                }
+            }
+
+            return result;
+        }
+
+        private int DoworkReturn2(int result)
+        {
+            Label1.Text = result.ToString();
+            return result + 1;
+        }
+        private int DoworkReturn3(int result)
+        {
+            Label2.Text = result.ToString();
+            return result + 1;
+        }
+
+        private void SetChekresult(int result)
+        {
+            Label1.Text = result.ToString();
+        }
+        private void SetChekresult2(int result)
+        {
+            Label2.Text = result.ToString();
+        }
+        
+    }
+}
---
+++ MultiThreadSampleWeb/WebForm1.aspx.designer.cs
@@ -0,0 +1,69 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace MultiThreadSampleWeb {
+    
+    
+    public partial class WebForm1 {
+        
+        /// <summary>
+        /// form1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Label1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Button1 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+        
+        /// <summary>
+        /// Label2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+        
+        /// <summary>
+        /// Button2 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+        
+        /// <summary>
+        /// Label3 コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+    }
+}
